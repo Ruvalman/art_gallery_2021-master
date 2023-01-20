765c3504abf3e9b8048e249b21a72d764e989d26
Ricardo Ruvalcaba <ricardo.ruvalcaba.ruiz@gmail.com>
2023-01-19 22:57:21 -0600

    First commit

---
+++ codedeploy/deploy_cmd.sh
@@ -0,0 +1,8 @@
+# If you have forked your own copy of the repo, replace repo account 
+# brandon-rich-aws-deployment-course with your own account name.
+aws deploy create-deployment \
+ --application-name art_gallery \
+ --deployment-config-name CodeDeployDefault.OneAtATime \
+ --deployment-group-name art_gallery_deployment_group \
+ --description "Demo Deployment" \
+ --github-location repository=brandon-rich-aws-deployment-course/art_gallery_2021,commitId=$(git rev-parse HEAD) \
