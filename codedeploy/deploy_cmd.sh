600caa34dc8800264a8586e824691b6ee0f73819
Ricardo Ruvalcaba <ricardo.ruvalcaba.ruiz@gmail.com>
2023-01-20 18:42:04 -0600

    test

--- codedeploy/deploy_cmd.sh
+++ codedeploy/deploy_cmd.sh
@@ -5,4 +5,4 @@ aws deploy create-deployment \
  --deployment-config-name CodeDeployDefault.OneAtATime \
  --deployment-group-name art_gallery_deployment_group \
  --description "Demo Deployment" \
- --github-location repository=Ruvalman/art_gallery_2021-master,commitId=$(git rev-parse HEAD) \
+ --github-location repository=ruvalman/art_gallery_2021-master,commitId=$(git rev-parse HEAD) \
